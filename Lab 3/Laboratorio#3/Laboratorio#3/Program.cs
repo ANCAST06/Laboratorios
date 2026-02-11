728a9e11ca4be68f67762221b26f52faa9932633
ANCAST06 <tkdhkwkv2m@privaterelay.appleid.com>
2026-02-11 09:42:33 -0600

    laboratorio 3 y 4

---
+++ Lab 3/Laboratorio#3/Laboratorio#3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, curso;
+            Console.WriteLine("Hello word");
+            Console.WriteLine("programa creado por Andres Castillo");
+            Console.WriteLine();
+            Console.Write("Escriba su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Escriba su curso: ");
+            curso = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Bienvenido " + nombre + " del curso: " + curso + ", Ten un excelente dia");
+            Console.WriteLine();
+            Console.Write("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 4/Laboratorio #4/Laboratorio #4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Comó te llamas?");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+
+            //Ejercicio 1
+            string Area_51;
+            int capacidad_de_carga;
+            float nivel_de_combustible;
+            bool activo_o_no_activo;
+
+            Console.WriteLine("Ingrese el nombre del modelo");
+            Area_51 = Console.ReadLine();
+
+            Console.WriteLine("Ingrese capacidad de carga");
+            capacidad_de_carga = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese nivel de combustible");
+           
+        }
+    }
+}
+
+
