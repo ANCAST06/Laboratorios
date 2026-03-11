445b5d332dc78823ef368ad69de50d5877a8cf23
ANCAST06 <tkdhkwkv2m@privaterelay.appleid.com>
2026-03-11 08:53:04 -0600

    Lab 8
    Se inicio el lab 8

---
+++ Lab 8/Laboratorio 8 AC 1195025/Laboratorio 8 AC 1195025/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_8_AC_1195025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Entrada de usuario
+            Console.Write("¿Cómo te llamas? ");
+            string nombre = Console.ReadLine();
+            // Salida de datos
+            Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
